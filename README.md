fa61d8654df85c02c5f731335c4a033e7e7a4569
Enda-Monks <enda.monks@gmail.com>
2022-10-07 20:50:53 -0400

    gonna try this with react instead :)

--- README.md
+++
@@ -1,3 +0,0 @@
-## SLAYERSTROLOGY
-
-in progress....
--- git_practice/src/app/app.module.ts
+++ git_practice/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//tools
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { ZodiacComponent } from './zodiac/zodiac.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    ZodiacComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
